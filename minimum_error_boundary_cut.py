2f47370b6283467a73c796f238bf32de077efc7d
Renato B. Arantes <renaaran@gmail.com>
2019-03-25 14:29:23 +0000

    Top+down image quilting

--- minimum_error_boundary_cut.py
+++ minimum_error_boundary_cut.py
@@ -38,7 +38,7 @@ class PatternsDataset(Dataset):
     
 class Pattern:
     def __init__(self, data, N):
-        self.data = data.astype(np.int16)
+        self.data = data.astype(np.int32)
         self.N = N
         
     def __eq__(self, other):
@@ -62,7 +62,9 @@ class CreatePattern:
     def __call__(self, t):
         i = t[0]
         j = t[1]
-        t = Pattern(self.sample.take(range(i,i+self.N),mode='raise', axis=0).take(range(j,j+self.N),mode='raise',axis=1), self.N)
+        t = Pattern(self.sample.take(range(i,i+self.N),mode='raise', axis=0).
+                                take(range(j,j+self.N),mode='raise',axis=1), 
+                                self.N)
 #        t_ref = Pattern(np.fliplr(t.data), self.N)
 #        t_rot = Pattern(np.rot90(t.data), self.N)
         return os.getpid(), set([t])
@@ -78,9 +80,9 @@ class Minimum_Cost_Path:
         assert blk1.shape[0] == blk2.shape[1]
         # get the overlap regions      
         block_size = blk1.shape[0]
-        self.ov1, self.ov2 = self.get_overlap(blk1, blk2, block_size, overlap_size, orientation)
         # calculate LE error for the overlap region
-        self.L2_error = self.calc_L2_error(self.ov1, self.ov2)
+        self.L2_error = self.calc_L2_error(blk1, blk2, block_size, 
+                                              overlap_size, orientation)
         # calculate the minimum cost matrix
         self.calc_cost() 
         # now calculate the minimum cost path
@@ -116,7 +118,7 @@ class Minimum_Cost_Path:
         return p
         
     def calc_cost(self):
-        self.cost = np.zeros(self.L2_error.shape, dtype=np.int)
+        self.cost = np.zeros(self.L2_error.shape, dtype=np.int32)
         # we don't need to calculate the first row
         self.cost[0,:] = self.L2_error[0,:]
         rows, cols = self.cost.shape        
@@ -131,37 +133,48 @@ class Minimum_Cost_Path:
             ov1 = blk1[:,block_size-overlap_size-2:] # right
             ov2 = blk2[:,:overlap_size+2] # left
         elif orientation == Orientation.BOTTOM_TOP:
-            ov1 = blk1[block_size-overlap_size-2:,:] # bottom
-            ov2 = blk2[:overlap_size+2,:] # top            
+            ov1 = np.transpose(blk1[block_size-overlap_size-2:,:], (1,0,2)) # bottom
+            ov2 = np.transpose(blk2[:overlap_size+2,:], (1,0,2)) # top            
         assert ov1.shape == ov2.shape
         return ov1, ov2
     
     @staticmethod
-    def calc_L2_error(ov1, ov2):
-        """
-        Returns the L2 norm on the overlaped regions
-         
-        ov1 : overlap region 1
-        ov2 : overlap region 2
-        """
+    def calc_L2_error(blk1, blk2, block_size, overlap_size, orientation):
+        ov1, ov2 = Minimum_Cost_Path.get_overlap(blk1, blk2, block_size, overlap_size, orientation)
         L2_error = np.sum((ov1-ov2)**2, axis=2)
+        assert (L2_error >= 0).all() == True
         return L2_error
 
-def join_blocks(blk1, blk2, path, debug=False):
+def join_horizontal_blocks(blk1, blk2, path, debug=False):
     G = [[0,255,0]] # green pixel
-    x = mcp.path[0]
-    y = blk1.shape[1]
-    concat = lambda i : np.concatenate((blk1[i,:y-overlap_size+x], G, blk2[i,x+1:])) \
-        if debug else np.concatenate((blk1[i,:y-overlap_size+x], blk2[i,x+1:]))
+    a = mcp.path[0]
+    b = blk1.shape[1]
+    concat = lambda i : np.concatenate((blk1[i,:b-overlap_size+a], G, blk2[i,a+1:])) \
+        if debug else np.concatenate((blk1[i,:b-overlap_size+a], blk2[i,a+1:]))
     c = concat(0)
-    res = np.zeros((block_size, len(c), 3), dtype=np.int)
+    res = np.zeros((block_size, len(c), 3), dtype=np.int32)
     res[0,:] = c
     for i in range(1, block_size):
-        x = mcp.path[i]-1
+        a = mcp.path[i]-1
         c = concat(i)
         res[i,:] = c
     return res
  
+def join_vertical_blocks(blk1, blk2, path, debug=False):
+    G = [[0,255,0]] # green pixel
+    a = mcp.path[0]
+    b = blk1.shape[0]
+    concat = lambda i : np.concatenate((blk1[:b-overlap_size+a,i], G, blk2[a+1:,i])) \
+        if debug else np.concatenate((blk1[:b-overlap_size+a,i], blk2[a+1:,i]))
+    c = concat(0)
+    res = np.zeros((len(c), block_size, 3), dtype=np.int32)
+    res[:,0] = c
+    for i in range(1, block_size):
+        a = mcp.path[i]-1
+        c = concat(i)
+        res[:,i] = c
+    return res
+    
 def PatternsFromSample(sample, N):
     patts = set()
     h, w, _ = sample.shape
@@ -184,27 +197,28 @@ def get_best(lst, blk1, N, orientation):
     sample = random.sample(lst, N)
     for patt in sample:
         blk2 = patt.data
-        ov1, ov2 = Minimum_Cost_Path.get_overlap(blk1, blk2, block_size, overlap_size, orientation)
-        mcp = Minimum_Cost_Path.calc_L2_error(ov1, ov2)
-        err = mcp.sum()
+        l2 = Minimum_Cost_Path.calc_L2_error(blk1, blk2, block_size, overlap_size, orientation)
+        err = l2.sum()
         pqe = (-err, blk2)
         if len(pq) < pq_N:
             heapq.heappush(pq, pqe)
         else:
-            try:
-                heapq.heappushpop(pq, pqe)
-            except ValueError as e:
-                print(e)
+            heapq.heappushpop(pq, pqe)
     return random.sample(pq, 1)[0][1]
 
+def join_blocks(blocks):
+        img = np.hstack(tuple(blocks))
+        return img
+            
 block_size = 30
 overlap_size = block_size//6
 number_of_tiles_in_output = 20 # output image widht in tiles
 
 if __name__ == '__main__':
-#    random.seed(42)
-    img_source = plt.imread('mesh.jpg')
-    img_output = np.zeros(img_source.shape, dtype=np.int)
+    random.seed(42)
+    np.random.seed(42)
+    img_source = plt.imread('apple.jpg')
+    img_output = np.zeros(img_source.shape, dtype=np.int32)
     plt.imshow(img_source)
     plt.show()
     
@@ -212,33 +226,49 @@ if __name__ == '__main__':
     random.shuffle(patterns)
     print('Number of patterns = {}'.format(len(patterns)))
     
+    ########## first row there is only left+right constraint
     img = get_random_pattern(patterns)
     img_dbg = img
     
-    ########## first row there is only left+right constraint
-    orientaion = Orientation.RIGHT_LEFT
-    for i in range(number_of_tiles_in_output):
+    for i in range(number_of_tiles_in_output-1):
         blk1 = img[:,img.shape[1]-block_size:]
-        blk2 = get_best(patterns, blk1, len(patterns), orientaion)
+        blk2 = get_best(patterns, blk1, len(patterns), Orientation.RIGHT_LEFT)
         # calculate the minimum cost path
-        mcp = Minimum_Cost_Path(blk1, blk2, overlap_size, orientaion)
+        mcp = Minimum_Cost_Path(blk1, blk2, overlap_size, Orientation.RIGHT_LEFT)
         # plot blocks side by side
-        img = join_blocks(img, blk2, mcp.path, debug=False)
-        img_dbg = join_blocks(img_dbg, blk2, mcp.path, debug=True)
-#    ########## second row there is left+right and top+bottom constraint
-#    for i in range(img.shape[1]//block_size):
-#        blk1 = img[:,(i*block_size):(i*block_size)+block_size]
-#        blk2 = get_best(patterns, blk1, len(patterns)//10, Orientation.BOTTOM_TOP)
-#        # calculate the minimum cost path
-#        mcp = Minimum_Cost_Path(blk1, blk2, overlap_size, Orientation.BOTTOM_TOP)
-#        # plot blocks side by side
-#        img = join_blocks(img, blk2, mcp.path, debug=False)
-#        img_dbg = join_blocks(img_dbg, blk2, mcp.path, debug=True)
+        img = join_horizontal_blocks(img, blk2, mcp.path, debug=False)
+        img_dbg = join_horizontal_blocks(img_dbg, blk2, mcp.path, debug=True)
 
     plt.figure(figsize=(10,10))
     plt.imshow(img_dbg)
     plt.show()
     
+    plt.figure(figsize=(10,10))
+    plt.imshow(img)
+    plt.show()
+        
+    ########## second row there is left+right and top+bottom constraint
+    
+    blks = []
+    blks_dbg = []
+    
+    orientation = Orientation.BOTTOM_TOP
+    for i in range(img.shape[1]//block_size):
+        blk1 = img[:,(i*block_size):(i*block_size)+block_size]        
+        blk2 = get_best(patterns, blk1, len(patterns), orientation)
+        # calculate the minimum cost path
+        mcp = Minimum_Cost_Path(blk1, blk2, overlap_size, orientation)
+        # plot blocks side by side
+        blks.append(join_vertical_blocks(blk1, blk2, mcp.path, debug=False))
+        blks_dbg.append(join_vertical_blocks(blk1, blk2, mcp.path, debug=True))
+
+    
+    img_dbg = join_blocks(blks_dbg)
+    plt.figure(figsize=(10,10))
+    plt.imshow(img_dbg)
+    plt.show()
+    
+    img = join_blocks(blks)
     plt.figure(figsize=(10,10))
     plt.imshow(img)
     plt.show()
